180eb3918428f3323b887593fb6eeb5703ae4b2c
Pietro Petitti <19675218+BambooFlower@users.noreply.github.com>
2020-06-08 10:15:41 +0100

    Add files via upload

---
+++ Code/Unity/Pong/Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour 
+{
+    public float speed = 8;
+    Vector3 targetPos;
+    Vector3 playerPos;
+    GameObject ballObj;
+
+    void Update()
+    {
+        ballObj = GameObject.FindGameObjectWithTag("Ball");
+        if(ballObj != null)
+        {
+            targetPos = Vector3.Lerp(transform.position, ballObj.transform.position, Time.deltaTime * speed);
+            playerPos = new Vector3(-20, Mathf.Clamp(targetPos.y, -13, 13), 0);
+            transform.position = new Vector3(20, playerPos.y, 0);
+        }
+    }
+}
---
+++ Code/Unity/Pong/Assets/Scripts/Paddle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour 
+{
+    public float paddleSpeed = 1;
+    public Vector3 playerPos;
+
+    void Update()
+    {
+        float yPos = transform.position.y + (Input.GetAxis("Vertical") * paddleSpeed);
+        playerPos = new Vector3(-20, Mathf.Clamp(yPos, -13, 13), 0);
+        transform.position = playerPos;
+    }
+}
